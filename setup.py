a7b273f6434c9b1db398c6852817aafe67685d1f
Jens Koetters <jkoetters5@gmail.com>
2018-03-13 08:43:15 +0100

    setup file for automatic installation of mysql.connector dependency

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup (
+    name = "dbnav",
+    version = "1.0",
+    description = "Database Navigation",
+    install_requires = ["mysql-connector==2.1.4"],
+)
+
